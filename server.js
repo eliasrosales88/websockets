cf5542e5529c19e44ff7a36b2f0cc57c607732e2
Elias Rosales <eliasrosales88@gmgmail.com>
2019-11-13 00:40:51 +0100

    Adding comunication between the server and clients

--- server.js
+++ server.js
@@ -11,4 +11,28 @@ app.get("/", (req, res, next)  => {
 server.listen(3000, () => {
   console.log("listening on port 3000");
   
-})
+});
+
+
+// Añadimos websockets
+const io = require("socket.io")(server);
+
+io.on('connection', socket  => {
+  // socket es una conexion con cada browser
+  console.log("Nueva conexión de un cliente");
+  
+  socket.on("nuevo-mensaje", data => { // mensaje recibido de un browser
+    console.log("Recibido, ", data);
+    
+    // emito desde el servidor a todos los browsers que esten conectados
+    io.emit("nuevo-mensaje", data);
+  })
+  
+
+  // setInterval(() => {
+  //   socket.emit("pasa un segundo")
+  // }, 1000)
+
+
+
+});
